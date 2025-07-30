eebf7985eb04296cb01998b44755edf26ae0b402
ggsong0328 <ggsong0328@gmail.com>
2025-07-30 09:11:50 +0900

    Load Products From Database

---
+++ lib/actions/product.actions.ts
@@ -0,0 +1,16 @@
+"user server";
+import { PrismaClient } from "@prisma/client";
+import { convertToPlainObject } from "../utils";
+import { LATEST_PRODUCTS_LIMIT } from "../constants";
+
+// Get lastest products
+export async function getLatestProducts() {
+  const prisma = new PrismaClient();
+
+  const data = await prisma.product.findMany({
+    take: LATEST_PRODUCTS_LIMIT,
+    orderBy: { createdAt: "desc" },
+  });
+
+  return convertToPlainObject(data);
+}
